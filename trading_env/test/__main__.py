e1791bc61e651713072482908c0cb4b049fbcf65
Yvictor <410175015@gms.ndhu.edu.tw>
2017-11-09 20:38:50 +0800

    env id

--- trading_env/test/__main__.py
+++ trading_env/test/__main__.py
@@ -6,7 +6,7 @@ import trading_env
 
 df = pd.read_csv('trading_env/test/data/SGXTWsample.csv', index_col=0, parse_dates=['datetime'])
 
-env = trading_env.make(envid='training_v1', obs_data_len=256, step_len=128,
+env = trading_env.make(env_id='training_v1', obs_data_len=256, step_len=128,
                       df=df, fee=0.1, max_position=5, deal_col_name='Price', 
                       feature_names=['Price', 'Volume', 
                                      'Ask_price','Bid_price', 
